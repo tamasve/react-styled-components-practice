e76f5060d123d102156a23334b17d1eff4e33594
tamasve <145750552+tamasve@users.noreply.github.com>
2023-12-03 02:11:02 +0100

    going on further in the doc

--- src/App.tsx
+++ src/App.tsx
@@ -3,103 +3,175 @@ import styled, { css } from 'styled-components';
 
 /**
  * https://styled-components.com/docs
- * Next part: Adapting based on props
  * 
  * Going through this doc practicing with my ideas...
+ * 1-2 Dec 2023
+ * Next part: Overriding .attrs
+ * 
+ * https://www.freecodecamp.org/news/styled-components-in-react/
+ * Next part: Props in Styled Components
  */
 
 
-function App() {
-    
+const Title = styled.h1`
+    font-family: Verdana;
+    font-size: 1.5em;
+    text-align: center;
+    color: blue;
+    margin-bottom: 2em;
+`
 
-    const Title = styled.h1`
-        font-family: Verdana;
-        font-size: 1.5em;
-        text-align: center;
-        color: blue;
-        margin-bottom: 2em;
-    `
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`
 
-    const Wrapper = styled.section`
-        padding: 4em;
-        background: papayawhip;
-    `
+// Using parameter (if it is a boolean, we can use it or leave it (? sign)...)
+// or using the || we can give default value in case we leave param
+const Button = styled.button<{ $primary?: boolean; $size?: string; }>`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid #BF4F74;
+    color: #BF4F74;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+    font-size: ${props => props.$size || "1em"};
 
-    // Using parameter (if it is a boolean, we can use it or leave it (? sign)...)
-    // or using the || we can give default value in case we leave param
-    const Button = styled.button<{ $primary?: boolean; $size?: string; }>`
-        background: transparent;
-        border-radius: 3px;
-        border: 2px solid #BF4F74;
-        color: #BF4F74;
-        margin: 0 1em;
-        padding: 0.25em 1em;
-        font-size: ${props => props.$size || "1em"};
+    ${props => props.$primary && css`
+        background: #BF4F74;
+        color: white;
+    `}
 
-        ${props => props.$primary && css`
-            background: #BF4F74;
-            color: white;
-        `}
-    `
-
-    // Using parameter version 2 - if it is a boolean, we can use it or leave it...
-    const Button2 = styled.button<{$primary: boolean;}>`
-        background: ${props => props.$primary ? "green" : "white"};
-        color: ${props => props.$primary ? "white" : "green"};
-        border-radius: 1em;
-        border: 2px solid green;
-        margin: 0 1em;
-        padding: 0.25em 1em;
-    `
-
-    // Using a number parameter:
-    const colors = ["white", "red", "green", "yellow", "blue", "black", "gray", "aquamarine", "magenta", "brown", "pink"];
-    const count = colors.length;
-
-    const MultiColoredButton = styled.button<{$color: number;}>`
-        color: ${props => colors[props.$color % count]};
-        background-color: ${props => colors[(props.$color + 3) % count]};
-        border-radius: 1em;
-        border: 2px solid ${props => colors[(props.$color + 6) % count]};
-        margin: 0 1em;
-        padding: 0.25em 1em;
-    `
-
-    // Extended style
-    const ExtButton = styled(Button)`
-        border-radius: 3em;
-        color: red;
-        border-color: red;
-    `
-    // This can work also for any non-styled-comp., but through the className handed over in props:
-    // const Link = ({ className, children }) => (
-    //     <a className={className}>
-    //       {children}
-    //     </a>
-    //   );
-
-    const ReversedButton = props => <Button {...props} children={props.children.split('').reverse()} />
-
-    const ButtonWithSpaces = props => <Button {...props} children={props.children.split('').join(" .")} />
+    &:hover {
+        background: lightblue;
+    }
+    &:active {
+        border: 4px solid red;
+    }
+    & + & {
+        background: linear-gradient(magenta,green);
+    }
+`
+// &: all instances of the component
+// & + &: directly next to
+// & ~ &: sibling, but maybe not directly next to
+// &.classname: if it also has classname class
+// .classname &: inside an element that has classname class
 
 
-    const TextInput = styled.textarea`
-        background-color: lightgreen;
-        border-radius: 1em;
-        font-size: 2rem;
-        padding: 0.5em;
-    `
-    const FlexMain = styled.main`
-        display: flex;
-        flex-direction: row;
-        gap: 1em;
-    `
+// Using parameter version 2 - if it is a boolean, we can use it or leave it...
+const Button2 = styled.button<{$primary: boolean;}>`
+    background: ${props => props.$primary ? "green" : "white"};
+    color: ${props => props.$primary ? "white" : "green"};
+    border-radius: 1em;
+    border: 2px solid green;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+`
 
-    const FlexSection = styled.section`
+// Using a number parameter:
+const colors = ["white", "red", "green", "yellow", "blue", "black", "gray", "aquamarine", "magenta", "brown", "pink"];
+const count = colors.length;
+
+const MultiColoredButton = styled.button<{$color: number;}>`
+    color: ${props => colors[props.$color % count]};
+    background-color: ${props => colors[(props.$color + 3) % count]};
+    border-radius: 1em;
+    border: 2px solid ${props => colors[(props.$color + 6) % count]};
+    margin: 0 1em;
+    padding: 0.25em 1em;
+`
+
+// Extended style
+const ExtButton = styled(Button)`
+    border-radius: 3em;
+    color: red;
+    border-color: red;
+`
+// This can work also for any non-styled-comp., but through the className handed over in props:
+// const Link = ({ className, children }) => (
+//     <a className={className}>
+//       {children}
+//     </a>
+//   );
+
+const ReversedButton = props => <Button {...props} children={props.children.split('').reverse()} />
+
+const ButtonWithSpaces = props => <Button {...props} children={props.children.split('').join(" .")} />
+
+
+const TextInput = styled.textarea`
+    background-color: lightgreen;
+    border-radius: 1em;
+    font-size: 2rem;
+    padding: 0.5em;
+`
+const FlexMain = styled.main`
+    display: flex;
+    flex-direction: row;
+    gap: 1em;
+`
+
+// A general section element with unique style (flex) for all children divs
+// + inner classes for making yellow background, thick green border (any children can have them)
+const FlexSection = styled.section`
     display: flex;
     flex-direction: column;
     gap: 1em;
+
+    .yellowBackgr {
+        background-color: yellow;
+    }
+
+    .greenBorder {
+        border: 3px solid green;
+    }
 `
+
+// Styled checkbox... attrs: constructor with attributes
+const CheckBox = styled.input.attrs({ type: "checkbox" })``;
+// ... with Label text styled using switch command and checking-dependent style
+// using &&: 1 instance:
+// ${CheckBox}:checked + && {...} = when the relevant checkbox gets checked this instance gets the following css...
+const LabelText = styled.label`
+    padding: 0.3em;
+    ${props => {
+        switch (props.$mode) {
+            case "dark":
+                return css`
+                    background-color: black;
+                    color: white;
+                    ${CheckBox}:checked + && {
+                        background-color: blue;
+                    }
+                `
+            case "light":
+                return css`
+                    background-color: transparent;
+                    color: black;
+                    ${CheckBox}: checked + && {
+                        color: red;
+                    }
+                `
+        }
+    }}
+`
+
+const DynSizedLabel = styled.label.attrs<{ $size: string; }>(
+    props => ({
+        $size: props.$size || "1em",
+        $num: props.$size.charAt(0)*1
+    })
+)`
+    font-size: ${props => props.$size};
+    padding: ${props => props.$size};
+    background-color: ${props => ["red", "green"][props.$num]}
+`
+// the last one does not work...
+
+
+function App() {
+
 
     // Extended style 2: using a style of a comp. for another (or copy?): ExtButton, 2nd appearance
     return (
@@ -110,6 +182,7 @@ function App() {
                 </Title>
             
                 <FlexSection>
+
                     <FlexSection>
                         <TextInput />
                         <div>
@@ -120,6 +193,7 @@ function App() {
                             <Button $size={"2em"}>Double size!</Button>
                         </div>
                     </FlexSection>
+
                     <FlexSection>
                         <TextInput />
                         <div>
@@ -135,6 +209,7 @@ function App() {
                             <ButtonWithSpaces>Nice to meet you, my dear Lady...</ButtonWithSpaces>
                         </div>
                     </FlexSection>
+
                     <FlexSection>
                         <h2>Awful parrot-style buttons :) demonstrate number parameter</h2>
                         <div>
@@ -143,6 +218,24 @@ function App() {
                             )}
                         </div>
                     </FlexSection>
+
+                    <FlexSection>
+                        <h2>Checkboxes with changes in label when checked...</h2>
+                        <div>
+                            <CheckBox />
+                            <LabelText $mode="dark">Dark</LabelText>
+                        </div>
+                        <div>
+                            <CheckBox />
+                            <LabelText $mode="light">Light</LabelText>
+                        </div>
+                        <h2>The elements with yellow background or thick green border (inner classes)</h2>
+                        <Button className='yellowBackgr'>Yellow</Button>
+                        <TextInput className='yellowBackgr greenBorder' />
+                    </FlexSection>
+
+                    <DynSizedLabel $size='2em'>Hello</DynSizedLabel>
+
                 </FlexSection>
             </Wrapper>
         </>
